6768647800d6622a2c6f448cd5f1c2e2dc30f398
Hassan <muhammad.hassan@delivermymeds.com>
2023-09-12 20:11:14 +0500

    Make billing page

--- routes/web.php
+++ routes/web.php
@@ -48,3 +48,7 @@ Route::get('/authorizations', function () {
 Route::get('/billing-report', function () {
     return view('billing-report');
 });
+
+Route::get('/billing', function () {
+    return view('billing');
+});
