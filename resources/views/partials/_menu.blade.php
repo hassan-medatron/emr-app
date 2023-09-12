067522b9a26f7fb0fe16235ad2c828654e3b7612
Hassan <muhammad.hassan@delivermymeds.com>
2023-09-13 02:42:10 +0500

    Refactor the menu bar implementing links using components

--- resources/views/partials/_menu.blade.php
+++ resources/views/partials/_menu.blade.php
@@ -1,228 +1,87 @@
 <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
     <div class="app-brand demo">
-      <a href="dashboard.html" class="app-brand-link">
-        <span class="app-brand-logo demo">
-          <img src="assets/img/medatron/logo.png" width="40">
-        </span>
-        <span class="app-brand-text demo menu-text ms-2">Medatron</span>
-      </a>
+        <a href="{{ route('dashboard') }}" class="app-brand-link">
+            <span class="app-brand-logo demo">
+                <img src="assets/img/medatron/logo.png" width="40">
+            </span>
+            <span class="app-brand-text demo menu-text ms-2">Medatron</span>
+        </a>
 
-      <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
-        <i class="bx bx-chevron-left bx-sm align-middle"></i>
-      </a>
+        <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
+            <i class="bx bx-chevron-left bx-sm align-middle"></i>
+        </a>
     </div>
 
     <div class="menu-inner-shadow"></div>
 
     <ul class="menu-inner py-1">
-      <!-- Dashboard -->
-      <li class="menu-item active">
-        <a href="dashboard.html" class="menu-link">
-          <i class="menu-icon tf-icons bx bx-home-circle"></i>
-          <div data-i18n="Analytics">Dashboard</div>
-        </a>
-      </li>
+        <x-menu-item :href="route('dashboard')" :active="request()->routeIs('dashboard')" iconClass="bx bx-home-circle">Dashboard</x-menu-item>
+        <x-menu-item :href="route('calendar')" :active="request()->routeIs('calendar')" iconClass="bx bx-calendar">Calendar</x-menu-item>
+        <x-menu-item :href="route('patients')" :active="request()->routeIs('patients')" iconClass="bx bx-user">Patients</x-menu-item>
+        <x-menu-item :href="route('orders')" :active="request()->routeIs('orders')" iconClass="bx bx-file">Orders</x-menu-item>
 
-      <li class="menu-item">
-        <a href="calendar.html" class="menu-link">
-          <i class="menu-icon tf-icons bx bx-calendar"></i>
-          <div data-i18n="Analytics">Calendar</div>
-        </a>
-      </li>
+        <x-menu-items-group :routesArray="[
+            'patient-report',
+            'revenue-report',
+            'referring-provider-report',
+            'orders-report',
+            'payment-report',
+            'inventory-report',
+            'billing-report',
+            'ar-aging-report',
+            'audit-report',
+        ]" iconClass="bx bxs-report" menuGroupName="Reports">
+            <x-menu-item :href="route('patient-report')" :active="request()->routeIs('patient-report')">Patients</x-menu-item>
+            <x-menu-item :href="route('revenue-report')" :active="request()->routeIs('revenue-report')">Revenue</x-menu-item>
+            <x-menu-item :href="route('referring-provider-report')" :active="request()->routeIs('referring-provider-report')">Referring Providers</x-menu-item>
+            <x-menu-item :href="route('orders-report')" :active="request()->routeIs('orders-report')">Orders</x-menu-item>
+            <x-menu-item :href="route('payment-report')" :active="request()->routeIs('payment-report')">Payments</x-menu-item>
+            <x-menu-item :href="route('inventory-report')" :active="request()->routeIs('inventory-report')">Inventory</x-menu-item>
+            <x-menu-item :href="route('billing-report')" :active="request()->routeIs('billing-report')">Billing</x-menu-item>
+            <x-menu-item :href="route('ar-aging-report')" :active="request()->routeIs('ar-aging-report')">Ar Aging</x-menu-item>
+            <x-menu-item :href="route('audit-report')" :active="request()->routeIs('audit-report')">Audit</x-menu-item>
+        </x-menu-items-group>
 
-      <li class="menu-item">
-        <a href="patients.html" class="menu-link">
-          <i class="menu-icon tf-icons bx bx-user"></i>
-          <div data-i18n="Analytics">Patients</div>
-        </a>
-      </li>
+        <x-menu-items-group :routesArray="['settings', 'users', 'organization', 'billing', 'inventory']" iconClass="bx bxs-cog" menuGroupName="Management">
+            <x-menu-item :href="route('settings')" :active="request()->routeIs('settings')">Settings</x-menu-item>
+            <x-menu-item :href="route('users')" :active="request()->routeIs('users')">Users</x-menu-item>
+            <x-menu-item :href="route('organization')" :active="request()->routeIs('organization')">Organization</x-menu-item>
+            <x-menu-item :href="route('billing')" :active="request()->routeIs('billing')">Billing</x-menu-item>
+            <x-menu-item :href="route('inventory')" :active="request()->routeIs('inventory')">Inventory</x-menu-item>
+        </x-menu-items-group>
 
-      <li class="menu-item">
-        <a href="orders.html" class="menu-link">
-          <i class="menu-icon tf-icons bx bx-file"></i>
-          <div data-i18n="Analytics">Orders</div>
-        </a>
-      </li>
+        <x-menu-items-group :routesArray="[
+            'invoices',
+            'payments',
+            'refunds',
+            'denials',
+            'eob-era',
+            'prescriptions',
+            'authorizations',
+            'cmns',
+            'deposits',
+        ]" iconClass="bx bx-dollar-circle" menuGroupName="Billing">
+            <x-menu-item :href="route('invoices')" :active="request()->routeIs('invoices')">Invoices</x-menu-item>
+            <x-menu-item :href="route('payments')" :active="request()->routeIs('payments')">Payments</x-menu-item>
+            <x-menu-item :href="route('refunds')" :active="request()->routeIs('refunds')">Refunds</x-menu-item>
+            <x-menu-item :href="route('denials')" :active="request()->routeIs('denials')">Denials</x-menu-item>
+            <x-menu-item :href="route('eob-era')" :active="request()->routeIs('eob-era')">EOB/ERA</x-menu-item>
+            <x-menu-item :href="route('prescriptions')" :active="request()->routeIs('prescriptions')">Prescriptions</x-menu-item>
+            <x-menu-item :href="route('authorizations')" :active="request()->routeIs('authorizations')">Authorizations</x-menu-item>
+            <x-menu-item :href="route('cmns')" :active="request()->routeIs('cmns')">CMNs</x-menu-item>
+            <x-menu-item :href="route('deposits')" :active="request()->routeIs('deposits')">Deposits</x-menu-item>
+        </x-menu-items-group>
 
-      <li class="menu-item">
-        <a href="javascript:void(0);" class="menu-link menu-toggle">
-          <i class="menu-icon tf-icons bx bxs-report"></i>
-          <div data-i18n="Layouts">Reports</div>
-        </a>
 
-        <ul class="menu-sub">
-          <li class="menu-item">
-            <a href="patient-report.html" class="menu-link">
-              <div data-i18n="Without menu">Patients</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="revenue-report.html" class="menu-link">
-              <div data-i18n="Without menu">Revenue</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="referring-provider-report.html" class="menu-link">
-              <div data-i18n="Without menu">Referring Providers</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="orders-report.html" class="menu-link">
-              <div data-i18n="Without menu">Orders</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="payment-report.html" class="menu-link">
-              <div data-i18n="Without menu">Payments</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="inventory-report.html" class="menu-link">
-              <div data-i18n="Without menu">Inventory</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="billing-report.html" class="menu-link">
-              <div data-i18n="Without menu">Billing</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="ar-aging-report.html" class="menu-link">
-              <div data-i18n="Without menu">AR Aging</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="audit-report.html" class="menu-link">
-              <div data-i18n="Without menu">Audit</div>
-            </a>
-          </li>
-        </ul>
-      </li>
+        <x-menu-items-group :routesArray="['items', 'purchase-order']" iconClass="bx bxs-dashboard" menuGroupName="Inventory">
+            <x-menu-item :href="route('items')" :active="request()->routeIs('items')">Items</x-menu-item>
+            <x-menu-item :href="route('purchase-order')" :active="request()->routeIs('purchase-order')">Purchase Orders</x-menu-item>
+        </x-menu-items-group>
 
-      <li class="menu-item">
-        <a href="javascript:void(0);" class="menu-link menu-toggle">
-          <i class="menu-icon tf-icons bx bx-cog"></i>
-          <div data-i18n="Layouts">Management</div>
-        </a>
-
-        <ul class="menu-sub">
-          <li class="menu-item">
-            <a href="settings.html" class="menu-link">
-              <div data-i18n="Without menu">Settings</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="users.html" class="menu-link">
-              <div data-i18n="Without menu">Users</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="organization.html" class="menu-link">
-              <div data-i18n="Without menu">Organization</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="billing.html" class="menu-link">
-              <div data-i18n="Without menu">Billing</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="inventory.html" class="menu-link">
-              <div data-i18n="Without menu">Inventory</div>
-            </a>
-          </li>
-        </ul>
-      </li>
-
-      <li class="menu-item">
-        <a href="javascript:void(0);" class="menu-link menu-toggle">
-          <i class="menu-icon tf-icons bx bx-dollar-circle"></i>
-          <div data-i18n="Layouts">Billing</div>
-        </a>
-
-        <ul class="menu-sub">
-          <li class="menu-item">
-            <a href="invoices.html" class="menu-link">
-              <div data-i18n="Without menu">Invoices</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="payments.html" class="menu-link">
-              <div data-i18n="Without menu">Payments</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="refunds.html" class="menu-link">
-              <div data-i18n="Without menu">Refunds</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="denials.html" class="menu-link">
-              <div data-i18n="Without menu">Denials</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="eob-era.html" class="menu-link">
-              <div data-i18n="Without menu">EOB/ERA</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="prescriptions.html" class="menu-link">
-              <div data-i18n="Without menu">Precriptions</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="authorizations.html" class="menu-link">
-              <div data-i18n="Without menu">Authorizations</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="cmns.html" class="menu-link">
-              <div data-i18n="Without menu">CMNs</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="deposits.html" class="menu-link">
-              <div data-i18n="Without menu">Deposits</div>
-            </a>
-          </li>
-        </ul>
-      </li>
-
-      <li class="menu-item">
-        <a href="javascript:void(0);" class="menu-link menu-toggle">
-          <i class="menu-icon tf-icons bx bxs-dashboard"></i>
-          <div data-i18n="Layouts">Inventory</div>
-        </a>
-
-        <ul class="menu-sub">
-          <li class="menu-item">
-            <a href="items.html" class="menu-link">
-              <div data-i18n="Without menu">Items</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="purchase-order.html" class="menu-link">
-              <div data-i18n="Without menu">Purchase Orders</div>
-            </a>
-          </li>
-        </ul>
-      </li>
-
-      <li class="menu-item">
-        <a href="javascript:void(0);" class="menu-link menu-toggle">
-          <i class="menu-icon tf-icons bx bx-git-pull-request"></i>
-          <div data-i18n="Layouts">Bitrix Automation</div>
-        </a>
-
-        <ul class="menu-sub">
-          <li class="menu-item">
-            <a href="#" class="menu-link">
-              <div data-i18n="Without menu">Without menu</div>
-            </a>
-          </li>
-        </ul>
-      </li>
+        <x-menu-items-group :routesArray="['dashboard']" iconClass="bx bx-git-pull-request" menuGroupName="Synchronization">
+            <x-menu-item :href="route('dashboard')" :active="request()->routeIs('dashboard')">Niko</x-menu-item>
+            <x-menu-item :href="route('dashboard')" :active="request()->routeIs('dashboard')">Bitrix</x-menu-item>
+        </x-menu-items-group>
 
     </ul>
-  </aside>
+</aside>
